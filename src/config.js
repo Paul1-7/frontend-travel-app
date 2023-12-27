d3f07931f4eaff5ce05c56ba6a47f9ded754dc67
Moises Tarifa <moicoxoxo@gmail.com>
2023-12-27 17:07:52 -0400

    add .env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+const BASE_DEV = import.meta.env.VITE_API_URL_BASE_DEV
+const BASE_PROD = import.meta.env.VITE_API_URL_BASE_PROD
+
 const config = {
   // basename: only at build time to set, and don't add '/' at end off BASENAME for breadcrumbs, also don't put only '/' use blank('') instead,
   // like '/berry-material-react/react/default'
@@ -5,10 +8,9 @@ const config = {
   defaultPath: '/administracion/rutas',
   fontFamily: `'Roboto', sans-serif`,
   borderRadius: 12,
-  BASE_URL: 'http://localhost:4000',
-  MAPBOX_ACCESS_TOKEN:
-    'pk.eyJ1IjoicGF1bDEtNyIsImEiOiJja29vdGdlcngwY2l4MndtbHIwdXo3dmI0In0.2fx3l_hLv_ELsBjcxUNc9g',
-  MAPBOX_URL_BASE: 'https://api.mapbox.com/directions/v5/mapbox'
+  BASE_URL: import.meta.env.PROD ? BASE_PROD : BASE_DEV,
+  MAPBOX_ACCESS_TOKEN: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
+  MAPBOX_URL_BASE: import.meta.env.VITE_MAPBOX_URL_BASE
 }
 
 export default config
